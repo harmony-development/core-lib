16ce410226e8deeba58bb103aaae9f766ac42455
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-09-20 22:47:11 +0300

    feat: encode / decode rgb, has_permission wasm

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,3 +1,3 @@
 //! `harmony-core` contains implementations for common operations in the Harmony protocol.
-pub mod permission;
 pub mod color;
+pub mod permission;
--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -1,16 +1,32 @@
 use js_sys::Map;
-use wasm_bindgen::{prelude::*, JsCast};
+use wasm_bindgen::prelude::*;
 
-/*#[wasm_bindgen]
+#[wasm_bindgen]
 pub fn has_permission(permissions: Map, query: &str) -> Option<bool> {
     let keys = permissions.keys().into_iter();
     let values = permissions.values().into_iter();
 
-    core::permission::has_permission(
-        keys.zip(values).flat_map(|(key, value)| {
+    let perms = keys
+        .zip(values)
+        .flat_map(|(key, value)| {
             let (key, value) = (key.ok()?, value.ok()?);
-            Some((key.dyn_ref::<JsString>()?.(), key.as_bool()?))
-        }),
-        query,
-    )
-}*/
+            Some((key.as_string()?, value.as_bool()?))
+        })
+        .collect::<Vec<_>>();
+
+    core::permission::has_permission(perms.iter().map(|(m, o)| (m.as_str(), *o)), query)
+}
+
+#[wasm_bindgen]
+pub fn encode_rgb(color: &[u8]) -> i32 {
+    core::color::encode_rgb([
+        color.get(0).copied().unwrap_or(0),
+        color.get(1).copied().unwrap_or(0),
+        color.get(2).copied().unwrap_or(0),
+    ])
+}
+
+#[wasm_bindgen]
+pub fn decode_rgb(color: i32) -> Box<[u8]> {
+    Box::new(core::color::decode_rgb(color))
+}
