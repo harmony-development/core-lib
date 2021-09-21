4f7cc80337759d2f613c36cc7c950682ba8ac71c
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-09-21 07:00:01 +0300

    feat: better has_permission interface

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -1,25 +1,53 @@
-use js_sys::Map;
-use wasm_bindgen::prelude::*;
+use js_sys::Array;
+use wasm_bindgen::{prelude::*, JsCast};
 
 #[wasm_bindgen]
-pub fn has_permission(permissions: Map, query: &str) -> Option<bool> {
-    let keys = permissions.keys().into_iter();
-    let values = permissions.values().into_iter();
+extern "C" {
+    #[wasm_bindgen(typescript_type = "Array<[string, bool]>")]
+    pub type PermissionArray;
+}
 
-    let perms = keys
-        .zip(values)
-        .flat_map(|(key, value)| {
-            let (key, value) = (key.ok()?, value.ok()?);
-            Some((key.as_string()?, value.as_bool()?))
-        })
-        .collect::<Vec<_>>();
+#[wasm_bindgen]
+pub fn has_permission(permissions: PermissionArray, query: &str) -> Result<Option<bool>, JsValue> {
+    let array = permissions
+        .dyn_into::<Array>()
+        .map_err(|_| "value not of type 'Array<[string, bool]>'")?;
 
-    core::permission::has_permission(perms.iter().map(|(m, o)| (m.as_str(), *o)), query)
+    let array_len = array.length() as usize;
+    let perms = array.iter().try_fold(
+        Vec::with_capacity(array_len),
+        |mut tot: Vec<(String, bool)>, value| {
+            let tuple: Array = value
+                .dyn_into()
+                .map_err(|_| "value not of type '[string, bool]'")?;
+            let mut tuple_iter = tuple.iter();
+
+            let key = tuple_iter
+                .next()
+                .ok_or("expected 'string' type value as first element of array")?;
+            let value = tuple_iter
+                .next()
+                .ok_or("expected 'bool' type value as second element of array")?;
+
+            let item = (
+                key.as_string().ok_or("expected value with type 'string'")?,
+                value.as_bool().ok_or("expected value with type 'bool'")?,
+            );
+            tot.push(item);
+
+            Result::<_, JsValue>::Ok(tot)
+        },
+    )?;
+
+    Ok(harmony_core::permission::has_permission(
+        perms.iter().map(|(m, o)| (m.as_str(), *o)),
+        query,
+    ))
 }
 
 #[wasm_bindgen]
 pub fn encode_rgb(color: &[u8]) -> i32 {
-    core::color::encode_rgb([
+    harmony_core::color::encode_rgb([
         color.get(0).copied().unwrap_or(0),
         color.get(1).copied().unwrap_or(0),
         color.get(2).copied().unwrap_or(0),
@@ -28,5 +56,5 @@ pub fn encode_rgb(color: &[u8]) -> i32 {
 
 #[wasm_bindgen]
 pub fn decode_rgb(color: i32) -> Box<[u8]> {
-    Box::new(core::color::decode_rgb(color))
+    Box::new(harmony_core::color::decode_rgb(color))
 }
