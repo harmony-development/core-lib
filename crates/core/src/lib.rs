dc786e865670af7c2938c4d6a68ed2636fac0a14
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-09-21 19:35:36 +0300

    fix: put wee_alloc in the correct library

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,9 +1,3 @@
 //! `harmony-core` contains implementations for common operations in the Harmony protocol.
 pub mod color;
 pub mod permission;
-
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
